4b4dddc120567012cb07a8369ebc9a240b741c8c
schnek <tolstikov.misha@gmail.com>
2017-03-03 01:20:54 +0300

    Added : random background.

---
+++ simple_register/background.php
@@ -0,0 +1,9 @@
+<?php
+    // array of filenames
+    $bg = array('bg-01.jpg', 'bg-02.jpg');
+    // generate random number size of the array
+    $i = rand(0, count($bg)-1);
+    // set variable equal to which random filename was chosen
+    $selectedBg = "$bg[$i]";
+?>  
+<style type="text/css"> body{ background: url(images/<?php echo $selectedBg; ?>) no-repeat; } </style>
