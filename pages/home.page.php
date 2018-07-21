ab04c1a3eef4ece7174eca6d75fcaee788960c16
Zyres <mr@eichbuehlerhof.de>
2018-07-21 21:54:09 +0200

    Style alignment for home.pgae.php

--- pages/home.page.php
+++ pages/home.page.php
@@ -4,19 +4,24 @@
 
 <?php include 'content/header.cont.php'; ?>
 
-<div class="container main">
-    <div class="row">
-        <div class="col-lg-8">
-            <?php include 'modules/slideshow/index.php'; ?>
-        </div>
-        <div class="col-md-4">
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<div class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <?php include 'modules/slideshow/index.php'; ?>
+            </div>
+            <div class="col-md-4">
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-12">
-            Some Content...
+</div>
+<div class="basic-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                Some Content...
+            </div>
         </div>
     </div>
 </div>
