3ae1cd1fc21e97f4175866b5a83bc6b177367ced
Zyres <mr@eichbuehlerhof.de>
2018-07-23 21:37:20 +0200

    Move news management stuff to a new page.

--- pages/admin/home.page.php
+++ pages/admin/home.page.php
@@ -19,31 +19,9 @@
 <div class="basic-padding">
     <div class="container">
         <div class="row">
-            <div class="col-md-5">
-                <h2>Write some news</h2>
-                <p>Let people know what is going on right now. Your Text will appear on the frontpage!</p>
-                <p>You should avoid any coloring and sizing since it gets overwritten with the default style of the page.</p>
-                <p>Feel free to add a picture to your message. It will appear in the default news image area on the home page.</p>
-            </div>
-            <div class="col-md-7">
-                <h2> News</h2>
-                <form>
-                    <input type="hidden" iname="actionType" value="1">
-                    <input type="hidden" name="userId" value="<?php echo Session::get('userid') ?>">
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="titel" placeholder="Titel">
-                    </div>
-                    <div class="form-group">
-                        <label for="upload">Upload lead image</label>
-                        <input type="file" class="form-control-file" id="upload">
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control" id="text" rows="15" placeholder="Message"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Post News</button>
-                    </div>
-                </form>
+            <div class="col-md-12">
+                <h2>Admin Actions</h2>
+                <a href="/admin/news" class="btn btn-dark btn-lg" role="button"><i class="far fa-newspaper"></i> News Management</a>
             </div>
         </div>
     </div>
---
+++ pages/admin/news.page.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+
+<?php include 'content/header.cont.php'; ?>
+
+<div class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Latest News</h2>
+                <hr>
+                <p>List the latest news inside a sortable table here... but, guess what... instead Mr. Poo is living here.</p>
+                <h1 align="center"><i class="fas fa-poo"></i></h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="basic-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <h2>Write some news</h2>
+                <p>Let people know what is going on right now. Your Text will appear on the frontpage!</p>
+                <p>You should avoid any coloring and sizing since it gets overwritten with the default style of the page.</p>
+                <p>Feel free to add a picture to your message. It will appear in the default news image area on the home page.</p>
+            </div>
+            <div class="col-md-7">
+                <h2> News</h2>
+                <form>
+                    <input type="hidden" iname="actionType" value="1">
+                    <input type="hidden" name="userId" value="<?php echo Session::get('userid') ?>">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="titel" placeholder="Titel">
+                    </div>
+                    <div class="form-group">
+                        <label for="upload">Upload lead image</label>
+                        <input type="file" class="form-control-file" id="upload">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" id="text" rows="15" placeholder="Message"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Post News</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'content/footer.cont.php'; ?>
