cc06eb223276a8c5875bdf4656adb63c75b75acd
Zyres <mr@eichbuehlerhof.de>
2018-07-23 21:10:14 +0200

    Removed base page for the admin panel Updated navigation to match latest rewrite rules

--- content/header.cont.php
+++ content/header.cont.php
@@ -156,9 +156,9 @@
                 <p style="text-align:left;">
                     <img src="<?php echo Config\Hosting::baseURL ?>uploads/avatars/<?php echo $userFields['avatar'] ?>" width="35px" height="35px" style="border:3px solid grey; vertical-align: middle;" > Welcome back, <?php echo $userFields['displayName'] ?>
                     <span style="float:right;">
-                        <a href="admin" class="btn btn-dark btn-sm" role="button" aria-pressed="true"><i class="fas fa-hand-spock"></i> Admin Panel</a>
-                        <a href="acp" class="btn btn-info btn-sm" role="button" aria-pressed="true"><i class="fas fa-cogs"></i> Account Panel</a>
-                        <a href="logout.php" class="btn btn-danger btn-sm" role="button" aria-pressed="true"><i class="fa fa-power-off"></i> Logout</a>
+                        <a href="/admin/home" class="btn btn-dark btn-sm" role="button" aria-pressed="true"><i class="fas fa-hand-spock"></i> Admin Panel</a>
+                        <a href="/acp" class="btn btn-info btn-sm" role="button" aria-pressed="true"><i class="fas fa-cogs"></i> Account Panel</a>
+                        <a href="<?php echo Config\Hosting::baseURL ?>logout.php" class="btn btn-danger btn-sm" role="button" aria-pressed="true"><i class="fa fa-power-off"></i> Logout</a>
                     </span>
                 </p>
             <?php
@@ -177,18 +177,18 @@
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <a class="navbar-brand" href="home">AEweb</a>
+          <a class="navbar-brand" href="<?php echo Config\Hosting::baseURL ?>">AEweb</a>
 
           <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
               <li class="nav-item active">
-                <a class="nav-link" href="home">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="test">Test</a>
+                <a class="nav-link" href="/test">Test</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="test2">Test2</a>
+                <a class="nav-link" href="/test2">Test2</a>
               </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
